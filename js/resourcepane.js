d29448caa403b90fc1b0c486557cb6df5c98dabd
ske66 <40204953@live.napier.ac.uk>
2019-01-14 17:56:41 +0000

    started the history list, array built with function, needs to be applied to the list

--- js/resourcepane.js
+++ js/resourcepane.js
@@ -1,91 +1,115 @@
 var current_tab = 0;
 var last_number = 1;
 var tabs = [];
+var web_history = [];
 
-function add_tab(load_id=null) {
-	if (load_id == null) {
+function add_tab(load_id = null) {
+    if (load_id == null) {
         var new_resource = add_resource(' ');
-    	tab_id = new_resource.id;
-	    add_resource_metadata(tab_id, 'title', '');
-    	localStorage.setItem("state",JSON.stringify(get_sd()));
-	} 
-    else { tab_id = load_id; }
+        tab_id = new_resource.id;
+        add_resource_metadata(tab_id, 'title', '');
+        localStorage.setItem("state", JSON.stringify(get_sd()));
+    } else {
+        tab_id = load_id;
+    }
 
     add_resource_header();
 
     var resource_type_idx = document.getElementById("resource_type").options.selectedIndex;
     var resource_type_txt = document.getElementById("resource_type").options[resource_type_idx].text;
     var resource_type_web = document.getElementById("resource_type").options[resource_type_idx].text;
-    if (resource_type_txt.toLowerCase() === "text") { add_text_resource_body(tab_id); }
-    if (resource_type_web.toLowerCase() === "web") { add_web_resource_body(tab_id); }
+    if (resource_type_txt.toLowerCase() === "text") {
+        add_text_resource_body(tab_id);
+    }
+    if (resource_type_web.toLowerCase() === "web") {
+        add_web_resource_body(tab_id);
+    }
 
-    set_active_tab(tab_id+"_body");
+    set_active_tab(tab_id + "_body");
     return tab_id
 }
 
 function add_resource_header() {
-  	tab_number = next_number();
+    tab_number = next_number();
     tabs.push(tab_id);
     var resource_pane_tab_head = $(`
-        <button id="`+ tab_id + `_btn" class="tablinks" 
-            onclick="set_active_tab('` + tab_id + `_body')">` + 
-            tab_number + `</button>
+        <button id="` + tab_id + `_btn" class="tablinks" 
+            onclick="set_active_tab('` + tab_id + `_body')">` +
+        tab_number + `</button>
     `);
 
     $(".resource_pane_tab_head").append(resource_pane_tab_head);
 }
 
 function load_tab(resource) {
-	if (resource != null) {
-		var tab_id = resource.id;
+    if (resource != null) {
+        var tab_id = resource.id;
         add_tab(tab_id);
         set_text_resource_title(tab_id, resource.metadata.title);
         set_text_resource_content(tab_id, resource.content);
-      //set_web_resource_title(tab_id, resource.metadata.title);
-      //set_web_resource_content(tab_id, resource.content;
-	}
-}
-
-function next_number(){ return last_number++; }
-
-function remove_tab(){
-    $("#"+current_tab+"_btn").remove();
-    $("#"+current_tab+"_body").remove();
-    var idx = tabs.indexOf(current_tab);
-	delete_resource(current_tab);
-    sd.nodes.forEach((node) => {
-		if (node.type == 'atom') {
-			if (current_tab in node.sources) {
-				delete_source(node.id, current_tab);
-			}
-		}
-    });
-	
-	localStorage.setItem("state",JSON.stringify(get_sd()));
-    if (tabs.length >= 1 && idx != -1)
-    {
-        tabs.splice(idx, 1);
-        current_tab = tabs[tabs.length-1];
-        set_active_tab(current_tab+"_body");
+        set_web_resource_title(tab_id, resource.metadata.title);
+        set_web_resource_content(tab_id, resource.content);
     }
 }
 
-function history_list() {
-    
+function next_number() {
+    return last_number++;
 }
+
+function remove_tab() {
+    $("#" + current_tab + "_btn").remove();
+    $("#" + current_tab + "_body").remove();
+    var idx = tabs.indexOf(current_tab);
+    delete_resource(current_tab);
+    sd.nodes.forEach((node) => {
+        if (node.type == 'atom') {
+            if (current_tab in node.sources) {
+                delete_source(node.id, current_tab);
+            }
+        }
+    });
+
+    localStorage.setItem("state", JSON.stringify(get_sd()));
+    if (tabs.length >= 1 && idx != -1) {
+        tabs.splice(idx, 1);
+        current_tab = tabs[tabs.length - 1];
+        set_active_tab(current_tab + "_body");
+    }
+}
+
+function web_search() {
+    var web_address = document.getElementById("webAddress_" + tab_id).value;
+
+    if (web_address.includes("http://") || web_address.includes("https://") || (web_address.includes("www."))) {
+        add_to_history(web_address);
+    } else {
+        console.log("This is not a valid web address");
+    }
+}
+
+function add_to_history(web_address) {
+    web_history.unshift(web_address);
+
+    if (web_history.length > 10) {
+        web_history.splice(10, 1);
+    }
+
+    console.log(web_history);
+}
+
 
 function remove_all_tabs() {
     var i = 0;
     tabs.forEach((tab) => {
-		$("#"+tabs[i]+"_btn").remove();
-		$("#"+tabs[i]+"_body").remove();
+        $("#" + tabs[i] + "_btn").remove();
+        $("#" + tabs[i] + "_body").remove();
         ++i;
     });
 }
 
 function set_active_tab(tab_id) {
-    if(tabs.length > 0){
-        current_tab = tab_id.substring( 0, tab_id.indexOf("_body") );
+    if (tabs.length > 0) {
+        current_tab = tab_id.substring(0, tab_id.indexOf("_body"));
         var i, tabcontent, tablinks;
         tabcontent = document.getElementsByClassName("resource_pane_tab_content");
         for (i = 0; i < tabcontent.length; i++) {
@@ -95,27 +119,23 @@ function set_active_tab(tab_id) {
         for (i = 0; i < tablinks.length; i++) {
             tablinks[i].className = tablinks[i].className.replace(" active", "");
         }
-		if (document.getElementById(tab_id)) {
-			document.getElementById(tab_id).style.display = "block";
-			document.getElementById(current_tab+"_btn").className += " active";
-		}
+        if (document.getElementById(tab_id)) {
+            document.getElementById(tab_id).style.display = "block";
+            document.getElementById(current_tab + "_btn").className += " active";
+        }
     }
 }
 
 function toggle_resource_pane() {
-    if(resource_pane_viewable_state == true)
-    {
-        $('#resource-pane').children().hide(); 
-        $('#resource-pane').hide(); 
+    if (resource_pane_viewable_state == true) {
+        $('#resource-pane').children().hide();
+        $('#resource-pane').hide();
         resource_pane_viewable_state = false;
-    }
-    else
-    {
-        $('#resource-pane').show(); 
+    } else {
+        $('#resource-pane').show();
         $('#resource-pane').children().show();
         resource_pane_viewable_state = true;
     }
     cy.center();
     cy.resize();
 }
-
--- js/resourcepane_web.js
+++ js/resourcepane_web.js
@@ -13,28 +13,34 @@ function add_web_resource_body(tab_id) {
                             <i class="fa fa-chevron-left fa-fw fa-lg"></i>
                         </button>
 
-                        <button type="button" class="btn btn-default" onclick="history_forward()" title="Go to next website">
+                        <button type="button" class="btn btn-default" title="Go to next website">
                             <i class="fa fa-chevron-right fa-fw fa-lg"></i>
                         </button>
 
-                        <button type="button" class="btn btn-default"
-                        onclick="history_list()" title="View previous website searches">
-                            <i class="fa fa-history fa-fw fa-lg"></i>
-                        </button>
-
-                </div>
-                <div class="form-group">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">History
+                                <i class="fa fa-chevron-down fa-fw">
+                                </i>
+                            </button>
+                            <div class="dropdown-menu">
+<a class="dropdown-item" href="#">Action<br></a>
+      <a class="dropdown-item" href="#">Another action<br></a>
+      <a class="dropdown-item" href="#">Something else here<br></a>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="form-group">
                     <label>Website Address</label>
 
-                    <textarea id="title_` + tab_id + `" type="text" rows="1" style="resize: none;" class="form-control" placeholder="Paste content web address..." onchange="change_title('` + tab_id + `')"></textarea> 
+                    <textarea id="webAddress_` + tab_id + `" type="text" rows="1" style="resize: none;" class="form-control" placeholder="Paste content web address..." onchange="change_title('` + tab_id + `')"></textarea> 
                     <label>Content</label>
-
 
 		<iframe id='test_iframe' class="form-control" src='https://crossorigin.me' style="resize: vertical; min-height:50vh;"></iframe>
 
                 </div>
                 <div type="button" class="btn btn-default">Refresh</div>
-<div type="button" class="btn btn-primary">Search</div>
+<div type="button" class="btn btn-primary" onclick="web_search()">Search</div>
             </form>
     </div>
     `);
@@ -53,7 +59,7 @@ function set_web_resource_content(tab_id, text) {
 }
 
 function change_title(tab_id) {
-    var title = document.getElementById("title_" + tab_id).value;
+    var title = document.getElementById("webAddress_" + tab_id).value;
     set_text_resource_title(tab_id, title)
 }
 
