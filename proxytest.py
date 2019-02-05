5ac89a4e5ce175015a20bc83f422467c75d45bc3
ske66 <40204953@live.napier.ac.uk>
2019-02-05 14:57:37 +0000

    fixed the iframe issue

--- proxytest.py
+++ proxytest.py
@@ -28,15 +28,15 @@ class RedirectHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(s):
         r = requests.get(URL)
         print(r.text)
-        if s.path in HTML_LIST:
-            s.serve_web()
-            print "HTML GOT"
-        else:
-            print "s:" + s.path
-            s.send_response(200)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            s.wfile.write(str(r.content))
+        s.serve_web()
+        #if s.path in HTML_LIST:
+            #s.serve_web()
+        #else:
+        print "s:" + s.path
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write(str(r.content))
 
     def get_page(s):
         r = requests.get(URL)
@@ -48,19 +48,19 @@ class RedirectHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         print "Writing html"
         s.send_header("Content-type", "text/html")
         s.end_headers()
-        s = open('static/js/resourcepane_web.js').read()
-        s = s.replace('//test', "successful")
+        #s = open('static/js/resourcepane_web.js').read()
+        #s = s.replace('//test', "successful")
         #s = s.replace('changethisurl', s.get_page())
-        f = open("static/js/resourcepane_web.js", 'w')
-        f.write(s)
-        f.close()
-        print "done"
-        #s.wfile.write("<html><head><title>Proxy Testing.</title></head>")
-        #s.wfile.write("<p>You accessed this path: %s</p>" % s.path)
-        #s.wfile.write("<iframe id='test_iframe' height='600' width='1000' srcdoc='" + s.get_page() + "'></iframe>")
-        #s.wfile.write("<button type='button' onclick='getSelectionText()'>textify</button>")
-        #s.wfile.write("<script>function getSelectionText() { var iframe= document.getElementById('test_iframe'); var idoc= iframe.contentDocument || iframe.contentWindow.document; var iwin= iframe.contentWindow || iframe.contentDocument.defaultView; console.log(''+iwin.getSelection() ) } </script>")
-        #s.wfile.write("</body></html>")
+        #f = open("static/js/resourcepane_web.js", 'w')
+        #f.write(s)
+        #f.close()
+        #print "done"
+        s.wfile.write("<html><head><title>Proxy Testing.</title></head>")
+        s.wfile.write("<p>You accessed this path: %s</p>" % s.path)
+        s.wfile.write("<iframe id='test_iframe' height='600' width='1000' srcdoc='" + s.get_page() + "'></iframe>")
+        s.wfile.write("<button type='button' onclick='getSelectionText()'>textify</button>")
+        s.wfile.write("<script>function getSelectionText() { var iframe= document.getElementById('test_iframe'); var idoc= iframe.contentDocument || iframe.contentWindow.document; var iwin= iframe.contentWindow || iframe.contentDocument.defaultView; console.log(''+iwin.getSelection() ) } </script>")
+        s.wfile.write("</body></html>")
 
 
     def escape(s, htmlstring):
