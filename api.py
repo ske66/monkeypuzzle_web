d49cfebbe5a2d7fe0589d8a13bb93eb30eb98187
ske66 <mark.barton99@hotmail.com>
2019-04-03 08:32:43 +0100

    Fully implemented CORS and Proxy now works via API call to linode server

--- api.py
+++ api.py
@@ -1,22 +1,18 @@
 from __future__ import print_function
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/api/v1/proxy', methods=['GET'])
 def api_id():
 
     headers = {'User-Agent': 'MonkeyPuzzle_Web_v1'}
 
-    address = request.args.get('address', '')
-    if address == '':
-        return ""
-    else:
-        new_address = address.replace('www.', 'https://', 1)
-        resp = requests.get(new_address, headers=headers)
+    address = request.args.get('address')
+    new_address = address.replace('www.', 'https://', 1)
+    resp = requests.get(new_address, headers=headers)
 
-        return jsonify(result=resp.text)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=true)
+    return jsonify(result=resp.text)
